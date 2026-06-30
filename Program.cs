27cf4eb94d1e4a40d0c10ccf914f00e898c55edc
Ehod <elhadjiousmane.diallo@ucad.edu.sn>
2026-06-30 10:55:46 +0000

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// DateOnly constructor parameters: 
+// new(year, month, day-of-month)
+//DateOnly holiday1 = new(2026, 12, 25); // Christmas Day
+DateOnly[] holidayDates = [
+	new(2026, 12, 25), // Christmas Day
+	new(2026, 12, 26), // Boxing Day 
+	new(2027, 1, 1),   // New Year's Day
+	new(2027, 3, 26),  // Good Friday
+	new(2027, 3, 29),  // Easter Monday
+	new(2027, 5, 3),   // Early May bank holiday
+	new(2027, 5, 31),  // Spring bank holiday
+	new(2027, 8, 30),  // Summer bank holiday
+	new(2027, 12, 25), // Christmas Day
+	new(2027, 12, 26) // Boxing Day 
+];
+Console.WriteLine("Holidays when the library is closed:");
+foreach (DateOnly date in holidayDates) {
+	Console.WriteLine(date.ToLongDateString());
+}
